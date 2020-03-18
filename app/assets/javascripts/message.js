6b83de9f7a6530e38269c9b75cb8aea82c045844
nakakouchi <61769004+nakakouchi@users.noreply.github.com>
2020-03-18 14:57:42 +0900

    自動更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,26 @@
 $(function(){
-  function buildHTML(message){
-    if ( message.image ) {
+  var buildHTML = function(message) {
+    if ( message.content && message.image ) {
       var html =
-        `<div class="message">
+        `<div class="message" data-message-id= ${message.id} >
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+            <img src=${message.image} class="lower-message__image>
+          </div>
+        </div>`
+  } else if (message.content) {
+    var html =
+      `<div class="message" data-message-id= ${message.id} >
           <div class="upper-message">
             <div class="upper-message__user-name">
               ${message.user_name}
@@ -16,29 +34,49 @@ $(function(){
               ${message.content}
             </p>
           </div>
-          <img src=${message.image} >
         </div>`
-      return html;
-    } else {
+    } else if (message.image) {
       var html =
-      `<div class="message">
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.created_at}
-            </div>
+      `<div class="message" data-message-id= ${message.id} >
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}
           </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
+          <div class="upper-message__date">
+            ${message.created_at}
           </div>
-        </div>`
-      return html;
+        </div>
+        <div class="lower-message">
+          <img src=${message.image} class="lower-message__image >
+        </div>
+      </div>`
     };
-  }
+    return html;
+  };
+
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message) 
+        });
+        $('.main_chat__messages').append(insertHTML);
+        $('.main_chat__messages').animate({ scrollTop: $('.main_chat__messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
@@ -63,4 +101,7 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   });
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
